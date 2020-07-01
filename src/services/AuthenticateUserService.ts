9652dc0b982e02aeb413c673a697e226299fc2a5
pikeno221 <gabriellgomesbh@gmail.com>
2020-07-01 09:02:30 -0300

    including jwt token on session response payload

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,16 +1,21 @@
-import { getCustomRepository, getRepository } from 'typeorm';
+import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
-import UserRepository from '../repositories/UserRepository';
 
 interface Request {
     email: string,
     password: string,
 }
 
+interface Response {
+    user: User,
+    token: string
+}
+
 class AuthenticateUserService {
-    public async execute({ email, password }: Request): Promise<{ user: User }> {
+    public async execute({ email, password }: Request): Promise<Response> {
 
         const userRepository = getRepository(User);
 
@@ -22,11 +27,15 @@ class AuthenticateUserService {
 
         if (!passwordMatched) throw new Error('email or password invalid. ');
 
+        const token = sign({}, '9d42cdfb23d59ad9cfa4545ee63629f0', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
         return {
             user,
-        }
-
-
+            token,
+        };
     }
 }
 export default AuthenticateUserService;
